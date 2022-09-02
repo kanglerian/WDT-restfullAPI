d9b27040539422fd00ffc213736728b88b5a0b2a
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2022-09-02 11:30:22 +0700

    add

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
-module.exports = 
+module.exports =
 {
   "development": {
     "username": DB_USERNAME,
@@ -17,10 +17,15 @@ module.exports =
     "dialect": "postgres"
   },
   "production": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": "postgres"
+    "username": "mnmwusdxpzhktj",
+    "password": "9ce337825961cd6c806fa7b5cc411cbdced5134196ba4a13d7719aa7e3258873",
+    "database": "dce2do5teqa7i2",
+    "host": "ec2-44-210-36-247.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
